71f486c9c0896e4008d092658b59262d58fde3e4
ngruixin <rn279@cornell.edu>
2017-12-01 00:04:49 -0500

    added test files, renamed stuff

--- Makefile
+++ Makefile
@@ -1,19 +1,22 @@
 test:
-	ocamlbuild -use-ocamlfind test_server.byte && ./test_server.byte -port ${PORT}
+	ocamlbuild -use-ocamlfind try_server.byte && ./try_server.byte -port ${PORT}
 #eg make PORT=9999 test
 
-tclient:
-	ocamlbuild -use-ocamlfind test_client.byte && ./test_client.byte -host ${HOST} -port ${PORT}
-#eg make HOST="127.0.0.1" PORT=9999 tclient
+client:
+	ocamlbuild -use-ocamlfind client_view.byte && ./client_view.byte -ip ${IP} -port ${PORT}
+#eg make HOST="127.0.0.1" PORT=9999 client
 
-tstate:
+test_state:
 	ocamlbuild -use-ocamlfind test_state.byte && ./test_state.byte
+
+test_client:
+	ocamlbuild -use-ocamlfind test_client.byte && ./test_client.byte
+
+test_server:
+	ocamlbuild -use-ocamlfind test_server.byte && ./test_server.byte
 
 server:
 	ocamlbuild -use-ocamlfind server.byte && ./server.byte -port ${PORT}
-
-client:
-	ocamlbuild -use-ocamlfind client.byte && ./client.byte -ip ${IP} -port ${PORT}
 
 check:
 	bash checkenv.sh
--- test_client.ml
+++ test_client.ml
@@ -1,82 +1,9 @@
-open Core
-open Async
+open OUnit2
 open Client
 
-let st = ref (init_state ())
+let tests = [
+]
 
-(* [handle_stdin str] forms the stringified client_input that the client sends
- * to the sever from the command line input [str] *)
-let handle_stdin str =
-  str
+let suite = "Client test suite" >::: tests
 
-(* [handle_resp str] takes [str] which is the stringified response from the
- * server, parses it to update its state or prints if necessary. *)
-let handle_resp str =
-  print_endline (str); ()
-
-let rec read r =
-  Reader.read_line r >>= function
-  | `Eof -> (printf "Server error, please try again. \n"; exit 0;)
-  | `Ok line -> (handle_resp line; read r)
-
-let rec send_msg w =
-  let stdin = Lazy.force Reader.stdin in
-  Reader.read_line stdin >>= function
-  | `Eof -> (printf "Error reading stdin\n"; return ())
-  | `Ok line ->
-    Writer.write_line w (handle_stdin line); send_msg w
-
-let rec create_user r w =
-  let stdin = Lazy.force Reader.stdin in
-  Reader.read_line stdin >>= function
-  | `Eof -> (printf "Error reading stdin\n"; create_user r w)
-  | `Ok line ->
-    Writer.write_line w (parse_create_user line);
-    read_create_username r w
-
-and read_create_username r w =
-  Reader.read_line r >>= function
-  | `Eof -> (printf "Error reading server\n"; create_user r w)
-  | `Ok line -> return (handle_create_user line r w)
-
-(* parse string of server response; on success, update state accordingly with
- * username; on failure, print error and loop create_user *)
-and handle_create_user res r w =
-  print_string res;
-  st := parse_receive res !st;
-  List.iter !st.print (fun str -> print_string str);
-(*if !st.userid = -1 then create_user r w else return ()*)
-  return ()
-
-let rw_loop r w _ =
-  don't_wait_for (send_msg w);
-  don't_wait_for (read r);
-  ()
-
-let chat _ r w =
-  create_user r w >>= fun x ->
-  rw_loop r w x;
-  Deferred.never ()
-
-let run ~host ~port =
-  let addr = Tcp.to_host_and_port host port in
-  ignore(Tcp.with_connection addr chat);
-  Deferred.never ()
-
-let main () =
-  print_string "Starting Caml Chat... \n";
-  print_string "Enter a username to begin: \n";
-  print_string "> ";
-  Command.async
-    ~summary:"Start the chat client"
-    Command.Spec.(
-      empty
-      +> flag "-host" (optional_with_default "127.0.0.1" string)
-        ~doc:" Port to listen on (default 9999)"
-      +> flag "-port" (optional_with_default 9999 int)
-        ~doc:" Port to listen on (default 9999)"
-    )
-    (fun host port () -> run ~host ~port)
-  |> Command.run
-
-let () = main ()
+let _ = run_test_tt_main suite
